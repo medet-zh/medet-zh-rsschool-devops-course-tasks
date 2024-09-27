90753286657dbab66dfeda08931f210ca5b46768
medet-zh <mzhanaidarov@rambler.ru>
2024-09-27 13:30:53 +0500

    TEST

--- main.tf
+++ main.tf
@@ -16,16 +16,3 @@ resource "aws_instance" "EC2_out_TF" {
 	instance_type                        = "t2.micro"
 	tags = local.common_tags
 }
-
-
-output "my_public_ip" {
-	value = aws_instance.my_ec2.public_ip
-}
-
-output "my_public_dns" {
-	value = aws_instance.my_ec2.public_dns
-}
-
-output "common_tags_output" {
-	value = local.common_tags
-}
