c5a0400294ebcf630017dce7f68d739477ee22bb
medet-zh <mzhanaidarov@rambler.ru>
2024-09-27 14:49:11 +0500

    updated

--- main.tf
+++ main.tf
@@ -22,3 +22,15 @@ resource "aws_instance" "EC2_out_TF_1" {
 	instance_type                        = "t2.micro"
 	tags = local.common_tags
 }
+
+resource "aws_instance" "EC2_out_TF_2" {
+	ami                                  = "ami-0ebfd941bbafe70c6"
+	instance_type                        = "t2.micro"
+	tags = local.common_tags
+}
+
+resource "aws_instance" "EC2_out_TF_3" {
+	ami                                  = "ami-0ebfd941bbafe70c6"
+	instance_type                        = "t2.micro"
+	tags = local.common_tags
+}
