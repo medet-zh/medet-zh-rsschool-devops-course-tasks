00e224548ecc85e24fe98e5c947ce72dc175bce5
medet-zh <mzhanaidarov@rambler.ru>
2024-09-27 14:47:48 +0500

    main

--- provider.tf
+++ provider.tf
@@ -1,3 +1,15 @@
+terraform {
+  
+  backend "s3" {
+    bucket = "terraform-state-bucket-medet-rs-school"
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
+
