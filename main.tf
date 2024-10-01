571e27499ae347b1fc346057d056bd5eacb59626
medet-zh <mzhanaidarov@rambler.ru>
2024-10-01 12:11:59 +0500

    update_Terraform

--- main.tf
+++ main.tf
@@ -1,18 +1,78 @@
-locals {
-	common_tags = {
-		Department = "LGC"
-		Project = "Certification"
+provider "aws" {
+  region = "us-east-2"
+}
+
+#VPC
+resource "aws_vpc" "rs_school_vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+
+#Public subnets
+resource "aws_subnet" "rs_school_public_subnets" {
+  count             = length(var.cidr_public_subnets)
+  vpc_id            = aws_vpc.rs_school_vpc.id
+  cidr_block        = element(var.cidr_public_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+  tags = {
+    Name = "RS_School_public_subnet_${count.index + 1}"
+  }
+}
+
+#Private subnets
+resource "aws_subnet" "rs_school_private_subnets" {
+  count             = length(var.cidr_private_subnets)
+  vpc_id            = aws_vpc.rs_school_vpc.id
+  cidr_block        = element(var.cidr_private_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+  tags = {
+    Name = "RS_School_private_subnet_${count.index + 1}"
+  }
+}
+
+
+#Internet Gateway
+resource "aws_internet_gateway" "rs_school_internet_gateway" {
+  vpc_id = aws_vpc.rs_school_vpc.id
+  tags = {
+    Name = var.ig_name
+  }
+}
+
+
+#Public Route Table
+resource "aws_route_table" "rs_school_public_route_table" {
+  vpc_id = aws_vpc.rs_school_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.rs_school_internet_gateway.id
+  }
+  tags = {
+    Name = var.public_route_table_name
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "rs_school_private_route_table" {
+	vpc_id = aws_vpc.rs_school_vpc.id
+	tags = {
+		Name = var.private_route_table_name
 	}
 }
 
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-0ebfd941bbafe70c6"
-  instance_type = "t2.micro"
-  tags = local.common_tags
+#Public Route Table and Public Subnet Association
+resource "aws_route_table_association" "Public_Subnet_Route_Table_Association" {
+  count          = length(aws_subnet.rs_school_public_subnets)
+  subnet_id      = aws_subnet.rs_school_public_subnets[count.index].id
+  route_table_id = aws_route_table.rs_school_public_route_table.id
 }
 
-resource "aws_instance" "EC2_out_TF" {
-	ami                                  = "ami-0ebfd941bbafe70c6"
-	instance_type                        = "t2.micro"
-	tags = local.common_tags
+#Private Route Table and Private Subnet Association
+resource "aws_route_table_association" "Private_Subnet_Route_Table_Association" {
+  count          = length(aws_subnet.rs_school_private_subnets)
+  subnet_id      = aws_subnet.rs_school_private_subnets[count.index].id
+  route_table_id = aws_route_table.rs_school_private_route_table.id
 }
